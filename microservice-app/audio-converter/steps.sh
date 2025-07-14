edcd9531ab3ea00abd85a10cb89cf712021d756b
Manohar-1305 <maddy98989@gmail.com>
2025-07-14 21:08:18 +0530

    Update steps.sh

--- microservice-app/audio-converter/steps.sh
+++ microservice-app/audio-converter/steps.sh
@@ -1,2 +1,5 @@
 docker build -t text-to-audio-app:latest .
 kind load docker-image text-to-audio-app:latest --name multi-node-cluster
+kubectl apply -f audio_deployment.yaml
+kubectl get pods
+kubectl get svc audio-service
