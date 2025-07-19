4acbda378c219634c76c6988c61599e2347308d8
Manohar-1305 <maddy98989@gmail.com>
2025-07-20 00:01:04 +0530

    Update steps.sh

--- microservice-app/gateway/steps.sh
+++ microservice-app/gateway/steps.sh
@@ -6,7 +6,7 @@ http://localhost:30000
 
 
 docker build -t microservice-gateway:latest .
-docker build -t audio-service:latest .
+docker build -t text-to-audio-app:latest .
 docker network create microservices-net
 
 docker run -d \
@@ -21,4 +21,23 @@ docker run -d \
   -p 5000:5000 \
   -e AUDIO_SERVICE_URL=http://audio-service:5003 \
   microservice-gateway:latest
+
+docker stop gateway audioservice
+docker rm gateway audioservice
+docker network create mynetwork
+
+docker run -d \
+  --name audioservice \
+  --network mynetwork \
+  -p 5003:5003 \
+  text-to-audio-app
+
+docker run -d \
+  --name gateway \
+  --network mynetwork \
+  -p 5000:5000 \
+  microservice-gateway
+
+  docker exec -it gateway curl http://audioservice:5003/convert
+
 
